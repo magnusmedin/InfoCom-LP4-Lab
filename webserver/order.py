f80c8e647248b94179fb9caf4351e2d42fba8594
scouw <magnus@medins.nu>
2022-04-08 14:34:31 +0200

    better uuid randomness

--- webserver/order.py
+++ webserver/order.py
@@ -3,7 +3,7 @@ import uuid
 class Order:
   def __init__(self, x_dest_osm, y_dest_osm):
     self.coordinates = (x_dest_osm, y_dest_osm)
-    self.order_uuid = uuid.uuid1()
+    self.order_uuid = uuid.uuid4()
     # when an order is placed we should check if there is an available drone, if so set status and drone accordingly
     self.status = "queue" 
     self.drone = None
